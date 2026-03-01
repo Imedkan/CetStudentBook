108a25e1c173461792f1a649a0ab8cdcd9bc33e6
Imedkan <Imedkan@users.noreply.github.com>
2026-03-01 22:00:46 +0300

    Add Book model

---
+++ CetStudentBook/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CetStudentBook.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 2)]
+        public string Name { get; set; } = string.Empty;   // book title
+
+        [Required]
+        [StringLength(200, MinimumLength = 2)]
+        public string Author { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public int PageCount { get; set; }
+
+        [Required]
+        public bool IsSecondHand { get; set; }
+    }
+}
