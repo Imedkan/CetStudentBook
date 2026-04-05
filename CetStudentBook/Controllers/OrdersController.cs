eec8a29c34e0f52fa5d726f2ba47e6e38d9a6f41
Imedkan <Imedkan@users.noreply.github.com>
2026-04-06 00:20:27 +0300

    Add OrdersController + MyOrders view; redirect Buy to MyOrders

---
+++ CetStudentBook/Controllers/OrdersController.cs
@@ -0,0 +1,32 @@
+﻿using CetStudentBook.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace CetStudentBook.Controllers
+{
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OrdersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> MyOrders()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+            var orders = await _db.Orders
+                .Include(o => o.Items)
+                .Where(o => o.UserId == userId)
+                .OrderByDescending(o => o.CreatedAt)
+                .ToListAsync();
+
+            return View(orders);
+        }
+    }
+}
